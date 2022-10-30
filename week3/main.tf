73a5e976b38be47be2ca0cbb1ff47d52f2bbacf5
Jinwoon gKim <97716160+jinwoongk@users.noreply.github.com>
2022-10-31 00:51:25 +0900

    delete week3 instance

--- week3/main.tf
+++ week3/main.tf
@@ -1,15 +1,15 @@
-locals {
-  env = replace(var.TFC_WORKSPACE_NAME, "t101-week3-", "")
-}
+# locals {
+#   env = replace(var.TFC_WORKSPACE_NAME, "t101-week3-", "")
+# }
 
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c76973fbe0ee100c"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "$(var.nickname)-t101-week3"
-  }
-}
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c76973fbe0ee100c"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "$(var.nickname)-t101-week3"
+#   }
+# }
