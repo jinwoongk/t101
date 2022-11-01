dba2927560a6d1df1de1309ac5a9376048abad85
Jinwoon gKim <97716160+jinwoongk@users.noreply.github.com>
2022-11-02 01:23:13 +0900

    add db variables

--- week3/variables.tf
+++ week3/variables.tf
@@ -14,21 +14,3 @@ variable "nickname" {
   default     = "jinwoong"
   description = "nick name"
 }
-
-variable "db_username" {
-  description = "The username for the database"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_password" {
-  description = "The password for the database"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_name" {
-  description = "The name to use for the database"
-  type        = string
-  default     = "tstudydb"
-}
