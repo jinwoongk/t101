fce4a04b908cb4124d3453cf8ad3b215fe4258e7
Jinwoon gKim <97716160+jinwoongk@users.noreply.github.com>
2022-11-02 23:38:16 +0900

    change variables

--- week3/modules/db/main.tf
+++ week3/modules/db/main.tf
@@ -18,4 +18,8 @@ resource "aws_db_instance" "myrds" {
   db_subnet_group_name   = aws_db_subnet_group.mydbsubnet.name
   vpc_security_group_ids = [module.network.aws_security_group.mysg2.id]
   skip_final_snapshot    = true
+  
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
 }
