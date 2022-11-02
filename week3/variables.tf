5389a99e906629d2447699521f1925a7775a41ef
Jinwoon gKim <97716160+jinwoongk@users.noreply.github.com>
2022-11-03 00:49:42 +0900

    remove workspace variables

--- week3/variables.tf
+++ week3/variables.tf
@@ -14,17 +14,6 @@ variable "nickname" {
   default     = "jinwoong"
   description = "nick name"
 }
-
-variable "db_user" {
-  description = "The username for the database"
-  type        = string
-}
-
-variable "db_pass" {
-  description = "The password for the database"
-  type        = string
-}
-
 variable "db_name" {
   description = "The name to use for the database"
   type        = string
